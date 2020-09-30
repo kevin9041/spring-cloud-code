f5160c8bcc4a3cff7df4129f101a8d06084ec36b
weihuanbo <635388281@qq.com>
2020-09-30 19:50:16 +0800

    4-5

--- ch4-5/ch4-5-provider/src/main/java/cn/springcloud/book/feign/controller/UserController.java
+++ ch4-5/ch4-5-provider/src/main/java/cn/springcloud/book/feign/controller/UserController.java
@@ -8,21 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public String addUser(User user , HttpServletRequest request){
-		String token=request.getHeader("oauthToken");
-		return "hello,"+user.getName();
-	}
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String addUser(User user, HttpServletRequest request) {
+        String token = request.getHeader("oauthToken");
+        return "hello," + user.getName();
+    }
 
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String updateUser( @RequestBody User user){
-		return "hello,"+user.getName();
-	}
-
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateUser(@RequestBody User user) {
+        return "hello," + user.getName();
+    }
 
 }
