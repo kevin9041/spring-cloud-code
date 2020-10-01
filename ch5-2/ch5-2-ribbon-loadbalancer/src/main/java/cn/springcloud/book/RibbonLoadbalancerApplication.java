d29a50f4ade22dec8d74e3275fa3d36a2fae017e
weihuanbo <635388281@qq.com>
2020-10-01 14:56:52 +0800

    5-2

--- ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/RibbonLoadbalancerApplication.java
+++ ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/RibbonLoadbalancerApplication.java
@@ -1,18 +1,16 @@
 package cn.springcloud.book;
 
+import cn.springcloud.book.config.AvoidScan;
+import cn.springcloud.book.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
-
-import cn.springcloud.book.config.AvoidScan;
-import cn.springcloud.book.config.TestConfiguration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
--- ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/config/TestConfiguration.java
+++ ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/config/TestConfiguration.java
@@ -8,10 +8,12 @@ import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 
+//貌似这个配置都没有生效？
 @Configuration
 @AvoidScan
 public class TestConfiguration {
-	
+
+	//IClientConfig注入失败
 	@Autowired
     IClientConfig config;
 
