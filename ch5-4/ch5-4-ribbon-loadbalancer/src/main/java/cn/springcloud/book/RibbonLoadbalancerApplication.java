b9de5f48bf45da92a5ba15cd250ff7a888be030d
weihuanbo <635388281@qq.com>
2020-10-01 15:52:01 +0800

    5-4

---
+++ ch5-4/ch5-4-ribbon-loadbalancer/src/main/java/cn/springcloud/book/RibbonLoadbalancerApplication.java
@@ -0,0 +1,21 @@
+package cn.springcloud.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class RibbonLoadbalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonLoadbalancerApplication.class, args);
+    }
+    
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ ch5-4/ch5-4-ribbon-loadbalancer/src/main/java/cn/springcloud/book/controller/TestController.java
@@ -0,0 +1,42 @@
+package cn.springcloud.book.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+	
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @Autowired
+    private LoadBalancerClient lbClient;
+
+	@GetMapping("/add")
+	public String add(Integer a, Integer b) {
+		return restTemplate.getForObject("http://CLIENT-A/add?a=" + a + "&b=" + b, String.class);
+	}
+	
+	@GetMapping("/add1")
+	public void add1(Integer a, Integer b) {
+		ServiceInstance instance = this.lbClient.choose("client-a");
+		System.out.println(instance.getHost()+":"+instance.getPort());
+	}
+	
+	@GetMapping("/add2")
+	public void add2(Integer a, Integer b) {
+		ServiceInstance instance = this.lbClient.choose("client-b");
+		System.out.println(instance.getHost()+":"+instance.getPort());
+	}
+
+	@GetMapping("/add3")
+	public void add3(Integer a, Integer b) {
+		ServiceInstance instance = this.lbClient.choose("client-c");
+		//没有获取可用实例返回null
+		System.out.println(instance.getHost()+":"+instance.getPort());
+	}
+}
